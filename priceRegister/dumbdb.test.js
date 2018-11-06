a6760bd1e07a3197c0b8dfe38fec2c025b71e99d
dasbo <kostadin.markov@gmail.com>
2018-11-06 13:01:50 +0200

    Improved unit tests

--- priceRegister/dumbdb.test.js
+++ priceRegister/dumbdb.test.js
@@ -10,30 +10,24 @@ console.log(ddb.valueOf());
 
 test("should add simple value", function(t) {
     const actual1 = ddb.put("kWun", "Wun");
-    const expected1 = "Wun";
-    t.is(actual1, expected1);
     const actual2 = ddb.get("kWun");
-    const expected2 = "Wun";
-    t.is(actual2, expected2);
+    const expected = "Wun";
+    t.is(actual1, actual2, expected);
     ddb.del("kWun");
 });
 
 test("should add json object", function(t) {
     const actual1 = ddb.put("kWun", { wun: 1 });
-    const expected1 = { wun: 1 };
-    t.deepEqual(actual1, expected1);
     const actual2 = ddb.get("kWun");
-    const expected2 = { wun: 1 };
-    t.deepEqual(actual2, expected2);
+    const expected = { wun: 1 };
+    t.deepEqual(actual1, actual2, expected);
     ddb.del("kWun");
 });
 
 test("should add json object with array", function(t) {
     const actual1 = ddb.put("kWun", { wun: [1, 2, 3] });
-    const expected1 = { wun: [1, 2, 3] };
-    t.deepEqual(actual1, expected1);
     const actual2 = ddb.get("kWun");
-    const expected2 = { wun: [1, 2, 3] };
-    t.deepEqual(actual2, expected2);
+    const expected = { wun: [1, 2, 3] };
+    t.deepEqual(actual1, actual2, expected);
     ddb.del("kWun");
 });
